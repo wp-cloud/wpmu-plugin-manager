b188312d6fc8dc6c0746da8876f253d70b05d505
Christian Foellmann <foellmann@foe-services.de>
2019-02-15 01:22:39 +0100

    only apply `remove_plugins` if WP_CLI is not used

--- wpmu-plugin-manager.php
+++ wpmu-plugin-manager.php
@@ -98,9 +98,12 @@ class WPC_PluginManager {
 		add_action( 'wpmu_update_blog_options', array( $this, 'blog_options_form_process' ) );
 
 		add_action( 'admin_init', array( $this, 'remove_plugin_update_row' ) );
-		
 		add_filter( 'plugin_row_meta' , array( $this, 'remove_plugin_meta' ), 10, 2 );
-		add_filter( 'all_plugins', array( $this, 'remove_plugins' ) );
+
+		if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+			add_filter( 'all_plugins', array( $this, 'remove_plugins' ) );
+		}
+
 		add_filter( 'plugin_action_links', array( $this, 'action_links' ), 10, 4 );
 		// add_filter( 'active_plugins', array( $this, 'check_activated' ) );
 
